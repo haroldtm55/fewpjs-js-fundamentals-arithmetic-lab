5691938575ed9c1242e4d91d70742f1aab711ae7
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-15 17:50:05 +1000

    Tests passed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const num1 = 31
 const num2 = 2
 const multiply = num1 * num2
 
-const random = Math.ceil(Math.random())
+const random = Math.ceil(Math.random()*10)
 
 const num3 = 29
 const num4 = 5
